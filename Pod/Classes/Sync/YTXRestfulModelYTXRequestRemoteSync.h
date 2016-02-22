4d601b998c9ff92892a60e42b0537900a3664669
caojun <78612846@qq.com>
2016-02-22 11:08:33 +0800

    升级到0.1.2重命名protocol

--- Pod/Classes/Sync/YTXRestfulModelYTXRequestRemoteSync.h
+++ Pod/Classes/Sync/YTXRestfulModelYTXRequestRemoteSync.h
@@ -6,7 +6,7 @@
 //  Copyright © 2016年 Elephants Financial Service. All rights reserved.
 //
 
-#import "YTXRestfulModelYTXRequestRemoteProtocol.h"
+#import "YTXRestfulModelRemoteProtocol.h"
 
 #import <Foundation/Foundation.h>
 
