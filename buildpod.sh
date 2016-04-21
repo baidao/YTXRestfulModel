6a9e8a30a06b6d49339d0fb8e92b8150776f14a9
caojun <78612846@qq.com>
2016-04-21 16:37:59 +0800

    Modify buildpod.sh

--- buildpod.sh
+++ buildpod.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR=$(pwd -L)
 popd > /dev/null
 
 echo "publish repo YTXRestfulModel"
-pod repo push baidao-ios-ytx-pod-specs YTXRestfulModel.podspec --verbose
+pod trunk push YTXRestfulModel.podspec --verbose
 
 ret=$?
 
